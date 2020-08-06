6f5a06644b10b681e43d32a60a3090b17f4aea7b
ritublog <68989503+ritublog@users.noreply.github.com>
2020-08-06 15:39:10 +0530

    Create exercise5.blade.php

---
+++ exercise5.blade.php
@@ -0,0 +1,34 @@
+// Data Type
+
+//string data type 
+
+<?php 
+$x = "Hello Ritu";
+$y = 'Hello Ritu';
+echo $x."<br>".$y;
+?>
+
+//integer data type
+
+<?php 
+$x = "20";
+# var_dump() returns the data type and value.
+var_dump($x);
+?>
+
+// float data type
+
+<?php 
+$x = "1.3";
+var_dump($x);
+?>
+
+// Null data type
+
+<?php
+$x = "Hello";
+$x = null;
+var_dump($x);
+?>
+
+
